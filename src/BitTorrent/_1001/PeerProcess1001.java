880d79b4760137a5d6a6cd4838464499a1cf56f0
dsk1020 <davidskrip1020@gmail.com>
2023-09-21 16:50:56 -0400

    Socket connection between 1001 and 1002. Code has issue where it requires to send to receive a message.

--- src/BitTorrent/_1001/PeerProcess1001.java
+++ src/BitTorrent/_1001/PeerProcess1001.java
@@ -1,9 +1,45 @@
 package BitTorrent._1001;
 
+import  java.net.*;
+import java.io.*;
+
 public class PeerProcess1001
 {
+
     public static void main(String[] args)
     {
+        try
+        {
+            ServerSocket servSocket = new ServerSocket(51234);
+            Socket _socket = servSocket.accept();
+            DataInputStream dis=new DataInputStream(_socket.getInputStream());
+            DataOutputStream dout=new DataOutputStream(_socket.getOutputStream());
+            BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
 
+            String msg = "";
+            String inMsg = "";
+            while(true)
+            {
+                msg = reader.readLine();
+                inMsg=dis.readUTF();
+
+                dout.writeUTF(msg);
+                System.out.println("\tIncoming= "+inMsg);
+
+                dout.flush();
+
+                if(inMsg.equals("null") || msg.equals("null"))
+                {
+                    break;
+                }
+            }
+            dout.close();
+            servSocket.close();
+
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error: " + e);
+        }
     }
 }
--- src/BitTorrent/_1002/PeerProcess1002.java
+++ src/BitTorrent/_1002/PeerProcess1002.java
@@ -1,9 +1,40 @@
 package BitTorrent._1002;
-
+import java.io.*;
+import  java.net.*;
 public class PeerProcess1002
 {
     public static void main(String[] args)
     {
+        try
+        {
+            Socket socket = new Socket("localHost", 51234);
+            DataInputStream dis=new DataInputStream(socket.getInputStream());
+            DataOutputStream dout=new DataOutputStream(socket.getOutputStream());
+            BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
 
+            String msg = "";
+            String inMsg = "";
+            while(true)
+            {
+                msg = reader.readLine();
+                dout.writeUTF(msg);
+                dout.flush();
+
+                inMsg=dis.readUTF();
+                System.out.println("\tIncoming= "+inMsg);
+
+                if(inMsg.equals("null") || msg.equals("null"))
+                {
+                    break;
+                }
+            }
+
+            dout.close();
+            socket.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
     }
 }
