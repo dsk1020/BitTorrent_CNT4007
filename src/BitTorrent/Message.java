f05e06a0a1a912c7c3b5b62555b14280cdf38e3b
dsk1020 <davidskrip1020@gmail.com>
2023-10-19 15:37:22 -0400

    Updated Message constructors

--- src/BitTorrent/Message.java
+++ src/BitTorrent/Message.java
@@ -21,10 +21,33 @@ public class Message
     {
         return this.msgLength;
     }
+
+    public void setMsgLength(int msgLength) {
+        this.msgLength = msgLength;
+    }
+
     public byte getMsgType()
     {
         return this.msgType;
     }
 
+    public void setMsgType(byte msgType) {
+        this.msgType = msgType;
+    }
 
+    public String getMsgBitfield() {
+        return msgBitfield;
+    }
+    public void setMsgBitfield(String bitfield)
+    {
+        this.msgBitfield = bitfield;
+    }
+
+    public List<Integer> getMsgPayload() {
+        return msgPayload;
+    }
+
+    public void setMsgPayload(List<Integer> msgPayload) {
+        this.msgPayload = msgPayload;
+    }
 }
